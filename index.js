6518ab87fbb59c8ee95d353b9b780763a5efdfe2
Maryam <maryam.mokhtari@gmail.com>
2018-12-10 12:37:25 +0330

    Change filename

---
+++ index.js
@@ -0,0 +1,19 @@
+const logger = require('./log')
+const log = require('./log').log
+const finalize = require('./utils/finalize')
+const dbConnect = require('./utils/dbConnect')
+const result = require('./utils/result')
+const token = require('./utils/token')
+const base = require('./utils/base')
+const outputResult = require('./utils/outputResult')
+
+module.exports = {
+  log,
+  logger,
+  finalize,
+  dbConnect,
+  result,
+  token,
+  base,
+  outputResult
+}
