533fedb9c0a9147a89e0d5519d882fcde7ca9a96
Maryam <maryam.mokhtari@gmail.com>
2018-12-19 11:01:24 +0330

    Some Enhancements

--- utils/base.js
+++ utils/base.js
@@ -4,6 +4,38 @@ const result = require('../utils/result')
 const token = require('../utils/token')
 const {finalize} = require('base')
 
+const getInfoBase = async function(functionName, tokenInput, info, isAdminConsidered, userId) {
+  console.log('getInfo:', tokenInput, functionName, userId, info);
+  let outputResult = new result()
+  try {
+    let tokenData;
+    if (tokenInput) {
+      tokenData = token.verify(tokenInput)
+      console.log('tokenData:', tokenData, );
+      if (!tokenData) {
+        outputResult.setErrorCode(403).setMessage('Authentication failed');
+        return outputResult;
+      }
+      if (userId && !tokenData.rolename.includes('admin')) {
+        outputResult.setErrorCode(403).setMessage('Permission denied');
+        return outputResult;
+      }
+    }
+    let params = tokenInput? isAdminConsidered? userId? [parseInt(tokenData.userId), parseInt(userId)]
+    : [null, parseInt(tokenData.userId)]
+    : info? [parseInt(tokenData.userId), ...info]:[parseInt(tokenData.userId)]
+    : []
+    console.log('functionName:', functionName, 'params:', params);
+    let dbOutput = await dbConnect.func(functionName, params);
+    console.log('dbOutput:', dbOutput);
+    outputResult.setCode(0).setData(dbOutput).setMessage('OK')
+  } catch (ex) {
+    outputResult.setErrorCode(-1).setMessage(ex.message);
+    log.error(`getInfo exception, ${ex.message}`);
+  }
+  return outputResult;
+}
+
 module.exports = {
   async saveInfo(tokenInput, info, functionName, userId, isAdminConsidered = false) {
    let outputResult = new result()
@@ -44,35 +76,4 @@ module.exports = {
    const result = getInfoBase(...args)
    finalize(result, res)
  },
- async getInfoBase(functionName, tokenInput, info, isAdminConsidered, userId) {
-   console.log('getInfo:', tokenInput, functionName, userId, info);
-   let outputResult = new result()
-   try {
-     let tokenData;
-     if (tokenInput) {
-       tokenData = token.verify(tokenInput)
-       console.log('tokenData:', tokenData, );
-       if (!tokenData) {
-         outputResult.setErrorCode(403).setMessage('Authentication failed');
-         return outputResult;
-       }
-       if (userId && !tokenData.rolename.includes('admin')) {
-         outputResult.setErrorCode(403).setMessage('Permission denied');
-         return outputResult;
-       }
-     }
-     let params = tokenInput? isAdminConsidered? userId? [parseInt(tokenData.userId), parseInt(userId)]
-     : [null, parseInt(tokenData.userId)]
-     : info? [parseInt(tokenData.userId), ...info]:[parseInt(tokenData.userId)]
-     : []
-     console.log('functionName:', functionName, 'params:', params);
-     let dbOutput = await dbConnect.func(functionName, params);
-     console.log('dbOutput:', dbOutput);
-     outputResult.setCode(0).setData(dbOutput).setMessage('OK')
-   } catch (ex) {
-     outputResult.setErrorCode(-1).setMessage(ex.message);
-     log.error(`getInfo exception, ${ex.message}`);
-   }
-   return outputResult;
- }
 }
